856a718a8191bda709bdd343d363ce3fe401790c
ArtoriasAbyssSlayer <harry.filis@yahoo.gr>
2023-08-01 20:32:47 +0300

    fix the requirements and the cuda hashgrid end

--- code/model/hash_encoder/backend.py
+++ code/model/hash_encoder/backend.py
@@ -3,16 +3,16 @@ from torch.utils.cpp_extension import load
 
 _src_path = os.path.dirname(os.path.abspath(__file__))
 
-_backend = load(name='_hash_encoder',
-                extra_cflags=['-O3', '-std=c++14'],
-                extra_cuda_cflags=[
-                    '-O3', '-std=c++14',
-                    '-U__CUDA_NO_HALF_OPERATORS__', #'-U__CUDA_NO_HALF_CONVERSIONS__', '-U__CUDA_NO_HALF2_OPERATORS__',
-                ],
-                sources=[os.path.join(_src_path, 'src', f) for f in [
-                    'hashencoder.cu',
-                    'bindings.cpp',
-                ]],
-                )
+_backend = load(
+    name='_hash_encoder',
+    sources=[os.path.join(_src_path, 'src', f) for f in [
+        'hashencoder.cu',
+        'bindings.cpp',
+    ]],
+    extra_cflags=['-O3'],
+    extra_cuda_cflags=['--use_fast_math'],
+    extra_ldflags=['-O3'],
+    verbose=True,  # Set verbose to True to enable detailed output.
+)
 
 __all__ = ['_backend']
