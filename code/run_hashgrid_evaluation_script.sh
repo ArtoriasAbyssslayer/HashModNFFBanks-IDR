e862919799748055b93fced5bdc5b5dcd7ead503
ArtoriasAbyssslayer <harry.filis@yahoo.gr>
2023-08-21 03:04:41 +0300

    eval command script

--- code/run_hashgrid_evaluation_script.sh
+++ code/run_hashgrid_evaluation_script.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python3 ./evaluation/eval.py --exps_folder exps --expname HashGrid --conf ./confs/embedder_conf_var/MultiResHash/dtu_fixed_cameras.conf --scan_id 65 --checkpoint latest --eval_rendering
+# python3 -u ./evaluation/eval --exps_folder exps --expname [EncodingNetName] --conf [EncodingNetConfigVariation] --scan_id [SCAN_ID] --checkpoint [latest] [--eval_rendering]
+python3 -u ./evaluation/eval.py --exps_folder exps --expname HashGrid --conf ./confs/embedder_conf_var/MultiResHash/dtu_fixed_cameras.conf --scan_id 65 --checkpoint latest --eval_rendering
