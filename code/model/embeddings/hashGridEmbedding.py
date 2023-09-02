gradient acc fixes

code/model/embeddings/hashGridEmbedding.py
@@ -91,7 +91,7 @@ class _HashGridMLP(nn.Module):
         xi = xi.unsqueeze(dim=-2) # (b..., 1, dim)
         xf = xf.unsqueeze(dim=-2) # (b..., 1, dim)
         # to match the input batch shape use bin_mask 
-        bin_mask = self.bin_mask.reshape((1,)*bdims + self.bin_mask.shape).detach()# (1..., neig, dim)
+        bin_mask = self.bin_mask.reshape((1,)*bdims + self.bin_mask.shape)# (1..., neig, dim)
         # get neighbors' indices and weights on each dim
         inds = torch.where(bin_mask, xi, xi+1)# (b..., neig, dim)
         ws = torch.where(bin_mask, 1-xf, xf)# (b...., neig, dim)

code/model/embeddings/nffb3d.py
@@ -58,20 +58,19 @@ class FourierFilterBanks(nn.Module):
                                                 periodic_fns=[torch.sin, torch.cos])
                 ff_enc_list.append(posenc_layer)
         if freq_enc_type == 'FourierFeatureNET':
-            nffb_lin_dims = [self.num_inputs] + [ff_enc_list[-1].embeddings_dim+1]*(self.grid_levels+1)
+            nffb_lin_dims = [self.num_inputs] + [ff_enc_list[-1].embeddings_dim+1]*(self.grid_levels)
         else:
-            nffb_lin_dims = [self.num_inputs] + [ff_enc_list[-1].embeddings_dim]*(self.grid_levels+1)
+            nffb_lin_dims = [self.num_inputs] + [ff_enc_list[-1].embeddings_dim]*(self.grid_levels)
         self.nffb_lin_dims = nffb_lin_dims
         self.ff_enc = nn.Sequential(*ff_enc_list)
         """ The Low - Frequency MLP part """
         self.n_nffb_layers = len(nffb_lin_dims)
         print(f"FFB Encoder Fourier Grid Filters: {self.n_nffb_layers}")
-        assert self.n_nffb_layers >= 5, "The NFFB  should have more than 5 layers"
+        assert self.n_nffb_layers >= 6, "The NFFB  should have more than 5 layers"
         # Input layer 
         setattr(self, "ff_lin" + str(0), nn.Linear(nffb_lin_dims[0], nffb_lin_dims[1]))
         for layer in range(1, self.n_nffb_layers - 1):
-            setattr(self, "ff_lin" + str(layer), nn.Linear(nffb_lin_dims[layer], nffb_lin_dims[layer + 1]))
-        
+            setattr(self, "ff_lin" + str(layer), nn.Linear(nffb_lin_dims[layer], nffb_lin_dims[layer + 1]).requires_grad_(True))
         """ Initialize parameters for Linear Layers"""
         # SDF network meaning we don't need to change the sine frequency(omega) for each layer -> ReLU is able to approximate the SDF but Wavelet need sine activation
         if layers_type == 'SIREN':
@@ -88,29 +87,31 @@ class FourierFilterBanks(nn.Module):
         self.feature_Vector_size  = out_layer_width
         # The ouput layers if SIREN branch selected or not - High Frequencies are Computed using Siren Layers Coherently with Fourier Grid Features 
         self.has_out = has_out
-        if has_out:
-            if self.include_input:
-                self.embeddings_dim = self.nffb_lin_dims[-1]  + self.num_inputs
-                """ The HIGH - Frequency MLP part """
-                for layer in range(0, self.grid_levels):
-                    setattr(self, "out_lin" + str(layer), nn.Linear(out_layer_width, self.nffb_lin_dims[-1]))
-                
-                if layers_type  == 'SIREN':  
-               
-                    self.out_layer = nn.Linear(out_layer_width,self.nffb_lin_dims[-1])
-                    self.out_activation = Sine(w0=self.sin_w0_high)
-                    self.init_SIREN_out()
-                elif layers_type  == 'ReLU':
-                    self.out_layer = nn.Linear(out_layer_width,self.nffb_lin_dims[-1])
-                    self.out_activation = nn.LeakyReLU(negative_slope=1e-2,inplace=False)
-                    self.init_ReLU_out()
-            else:
-                self.out_layer = nn.Linear(out_layer_width,self.nffb_lin_dims[-1])
+        
+        if self.include_input:
+            self.embeddings_dim = self.nffb_lin_dims[-1] + self.num_inputs
         else:
-            if self.include_input:
-                self.embeddings_dim = self.nffb_lin_dims[-1] + self.num_inputs
-            else:
-                self.embeddings_dim = self.nffb_lin_dims[-1] 
+            self.embeddings_dim = self.nffb_lin_dims[-1] 
+        if has_out:
+        
+        
+            """ The HIGH - Frequency MLP part """
+            for layer in range(0, self.grid_levels):
+                setattr(self, "out_lin" + str(layer), nn.Linear(out_layer_width, self.nffb_lin_dims[-1]).requires_grad_(True))
+            
+            if layers_type  == 'SIREN':  
+            
+                self.out_layer = nn.Linear(out_layer_width,self.nffb_lin_dims[-1]).requires_grad_(True)
+                self.out_activation = Sine(w0=self.sin_w0_high)
+                self.init_SIREN_out()
+            elif layers_type  == 'ReLU':
+                self.out_layer = nn.Linear(out_layer_width,self.nffb_lin_dims[-1])
+                self.out_activation = nn.LeakyReLU(negative_slope=1e-2,inplace=False)
+                self.init_ReLU_out()
+        else:
+            self.out_layer = nn.Linear(out_layer_width,self.nffb_lin_dims[-1]).requires_grad_(True)
+
+            
         # Attention Module
         # self.multi_head_attention = MultiHeadAttentionModule(self.feature_Vector_size,num_heads=4)
         # self.StyleModulationBlock = StyleModulation(self.n_levels,self.feature_Vector_size)
@@ -151,10 +152,8 @@ class FourierFilterBanks(nn.Module):
         for layer in range(0,self.n_nffb_layers-1):
             ff_lin = getattr(self,'ff_lin' + str(layer)).to(x.device)
             x = ff_lin(x)
-            x = self.lin_activation(x)
-            
+            x = self.lin_activation(x)     
             if layer > 0:
-                
                 embed_Feat = embeddings_list[layer-1] + x
                 # Apply Style Modulation
                 # embed_Feat= self.StyleModulationBlock(x,embed_Feat[layer-1])
@@ -169,6 +168,8 @@ class FourierFilterBanks(nn.Module):
                     x_high = self.out_activation(x_high)
                     x_out = x_out + x_high
                 else:
+                    lin_out = self.out_layer.to(x.device)
+                    embed_Feat = lin_out(embed_Feat)
                     features_list.append(embed_Feat)
 
         if self.has_out:
@@ -231,7 +232,3 @@ class FourierFilterBanks(nn.Module):
         for layer in range(self.grid_levels):
             lin = getattr(self, "out_lin" + str(layer)) 
             sine_init(lin,self.sin_w0_high)
-    
-    " optimizer utils "
-    def get_optimizer(self,lr,weight_decay):
-        return torch.optim.Adam(self.parameters(),lr=lr,weight_decay=weight_decay)  

code/model/implicit_differentiable_renderer.py
@@ -53,33 +53,9 @@ class ImplicitNetwork(nn.Module):
                 embed_fn, input_ch = get_embedder(multires)
                 self.embed_fn = embed_fn
                 dims[0] = input_ch     
-        # if embed_type == 'FFB':
-        #     FFB_Net_Config={
-        #             'include_input': True,
-        #             'in_dim': d_in,
-        #             'embed_type': 'HashGridTcnn',
-        #             'network_dims': dims,
-        #             'n_levels': multires,
-        #             'max_points_per_level': max_points_per_entry,
-        #             'log2_hashmap_size': log2_max_hash_size,
-        #             'base_resolution': base_resolution,
-        #             'desired_resolution': desired_resolution,
-        #             "base_sigma": 8.0,
-        #             "exp_sigma": 1.26,
-        #             "grid_embedding_std": 0.0001,
-        #             'per_level_scale': 2.0,
-        #             'freq_enc_type':'PositionalEncodingNET',
-        #             'has_out':True,
-        #             'bound': bound,
-        #             'layers_type':'ReLU'
-        #         }
-        #     self.NFFB_SDF = NFFB(FFB_Net_Config)
-        #     self.softplus = nn.Softplus(beta=100)
-        # else:
         #---- Classic IDR Implicit Geometric Reguralization Network ----#
         self.num_layers = len(dims)
         self.skip_in = skip_in
-
         for l in range(0, self.num_layers - 1):
             if l + 1 in self.skip_in:
                 out_dim = dims[l + 1] - dims[0]
@@ -87,7 +63,6 @@ class ImplicitNetwork(nn.Module):
                 out_dim = dims[l + 1]
 
             lin = nn.Linear(dims[l], out_dim)
-
             if geometric_init:
                 if l == self.num_layers - 2:
                     torch.nn.init.normal_(lin.weight, mean=np.sqrt(np.pi) / np.sqrt(dims[l]), std=0.0001)
@@ -112,12 +87,6 @@ class ImplicitNetwork(nn.Module):
         self.softplus = nn.Softplus(beta=100)
 
     def forward(self, input, compute_grad=False):
-        # "NFFB network Forward Call"
-        # if self.embed_type == 'FFB':
-        #     x = self.NFFB_SDF(input)
-        #     x = self.softplus(x)
-        #     x[...,0] = F.tanh(x[...,0]/2)
-        #     return x 
         " IDR Resnet - With Clamped Output"
         if self.embed_fn is not None:
             input = self.embed_fn(input)
@@ -280,7 +249,6 @@ class IDRNetwork(nn.Module):
                                                                  object_mask=object_mask,
                                                                  ray_directions=ray_dirs)
         self.implicit_network.train()
-
         points = (cam_loc.unsqueeze(1) + dists.reshape(batch_size, num_pixels, 1) * ray_dirs).reshape(-1, 3)
 
         sdf_output = self.implicit_network(points)[:, 0:1]
@@ -309,6 +277,9 @@ class IDRNetwork(nn.Module):
             surface_sdf_values = output[:N, 0:1].detach()
 
             g = self.implicit_network.gradient(points_all)
+            torch.cuda.empty_cache()
+            # Avoid OOM issue 
+            gc.collect()
             surface_points_grad = g[:N, 0, :].clone().detach()
             grad_theta = g[N:, 0, :]
 
@@ -338,14 +309,13 @@ class IDRNetwork(nn.Module):
             'object_mask': object_mask,
             'grad_theta': grad_theta
         }
-        # Avoid OOM issue 
-        torch.cuda.empty_cache()
-        gc.collect()
+        
         return output
 
     def get_rbg_value(self, points, view_dirs):
         output = self.implicit_network(points)
         g = self.implicit_network.gradient(points)
+        torch.cuda.empty_cache()
         normals = g[:, 0, :]
 
         feature_vectors = output[:, 1:]

code/model/ray_tracing.py
@@ -204,7 +204,7 @@ class RayTracing(nn.Module):
         pts_intervals = pts_intervals.reshape((-1, self.n_steps))[sampler_mask]
 
         sdf_val_all = []
-        for pnts in torch.split(points.reshape(-1, 3), 100000, dim=0):
+        for pnts in torch.split(points.reshape(-1, 3), 10000, dim=0):
             sdf_val_all.append(sdf(pnts))
         sdf_val = torch.cat(sdf_val_all).reshape(-1, self.n_steps)
 

code/training/idr_train.py
@@ -101,12 +101,14 @@ class IDRTrainRunner():
         self.train_dataloader = torch.utils.data.DataLoader(self.train_dataset,
                                                             batch_size=self.batch_size,
                                                             shuffle=True,
-                                                            collate_fn=self.train_dataset.collate_fn
+                                                            collate_fn=self.train_dataset.collate_fn,
+                                                            pin_memory=True
                                                             )
         self.plot_dataloader = torch.utils.data.DataLoader(self.train_dataset,
                                                            batch_size=self.conf.get_int('plot.plot_nimgs'),
                                                            shuffle=True,
-                                                           collate_fn=self.train_dataset.collate_fn
+                                                           collate_fn=self.train_dataset.collate_fn,
+                                                           pin_memory=True
                                                            )
 
         